0aab6d99d6928e6148b4664e6c201436485f5efe
Patrick Collins <PatrickCP0@Gmail.com>
2019-01-22 22:41:15 -0500

    merge and etc

---
+++ .config/wpg/formats/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#eeeeee", "#212121" },
+	[SchemeSel] = { "#eeeeee", "#d50000" },
+	[SchemeOut] = { "#eeeeee", "#18ffff" },
+};
