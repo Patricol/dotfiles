01c3fa83939489cea33cf138e9c02fd129748f6a
Patrick Collins <PatrickCP0@Gmail.com>
2023-09-15 12:26:18 -0600

    vc fixing etc

---
+++ .dot/bin/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P0212121
+                 \e]P1d50000
+                 \e]P200c853
+                 \e]P3ffd600
+                 \e]P42962ff
+                 \e]P5aa00ff
+                 \e]P600b8d4
+                 \e]P7bdbdbd
+                 \e]P8616161
+                 \e]P9ff5252
+                 \e]PA69f0ae
+                 \e]PBffff00
+                 \e]PC448aff
+                 \e]PDe040fb
+                 \e]PE18ffff
+                 \e]PFeeeeee
+                 \ec'
